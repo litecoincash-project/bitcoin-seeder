fa9f3d104bd9e385cf3d639b06483082b4c78e5b
Carlos Melo <carlosmmelo@gmail.com>
2018-06-13 18:50:05 -0400

    Implementing for XSN Core Network

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ public:
   CDnsSeedOpts() : nThreads(96), nDnsThreads(4), nPort(53), mbox(NULL), ns(NULL), host(NULL), tor(NULL), fUseTestNet(false), fWipeBan(false), fWipeIgnore(false), ipv4_proxy(NULL), ipv6_proxy(NULL) {}
 
   void ParseCommandLine(int argc, char **argv) {
-    static const char *help = "litecoincash-seeder\n"
+    static const char *help = "xsn-seeder\n"
                               "Usage: %s -h <host> -n <ns> [-m <mbox>] [-t <threads>] [-p <port>]\n"
                               "\n"
                               "Options:\n"
@@ -397,7 +397,7 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"seeds.litecoinca.sh", ""};
+static const string mainnet_seeds[] = {"45.63.38.173", ""};
 static const string testnet_seeds[] = {"127.0.0.1", ""};
 static const string *seeds = mainnet_seeds;
 
@@ -455,10 +455,10 @@ int main(int argc, char **argv) {
   bool fDNS = true;
   if (opts.fUseTestNet) {
       printf("Using testnet.\n");
-      pchMessageStart[0] = 0xb6;
-      pchMessageStart[1] = 0xf5;
-      pchMessageStart[2] = 0xd3;
-      pchMessageStart[3] = 0xcf;
+      pchMessageStart[0] = 0xce;
+      pchMessageStart[1] = 0xe2;
+      pchMessageStart[2] = 0xca;
+      pchMessageStart[3] = 0xff;
       seeds = testnet_seeds;
       fTestNet = true;
   }
